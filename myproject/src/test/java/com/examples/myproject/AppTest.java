f121f0c034ee4c55582ec3c64dc1ed2785908d1a
DESKTOP-VAU504C\Benefind-dev <riccardo.sgroi@benefind.it>
2024-05-24 19:05:14 +0200

    My first commit

---
+++ myproject/src/test/java/com/examples/myproject/AppTest.java
@@ -0,0 +1,52 @@
+package com.examples.myproject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AppTest {
+	private App app;
+	
+	@Before
+	public void setup() {
+		app = new App();
+	}
+	
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
+//public class AppTest 
+//    extends TestCase
+//{
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//}
+
+
