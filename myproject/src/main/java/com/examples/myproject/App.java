33c7035e6f140678c601aa30ca0edfc743cda831
DESKTOP-VAU504C\Benefind-dev <riccardo.sgroi@benefind.it>
2024-05-26 11:00:50 +0200

    add method to App class

--- myproject/src/main/java/com/examples/myproject/App.java
+++ myproject/src/main/java/com/examples/myproject/App.java
@@ -14,4 +14,11 @@ public class App
     public String sayHello() {
     	return "Hello";
     }
+    
+    public String sayHello(String name) {
+    	if (name == null)
+    		return "Hello";
+    	else
+    		return "Hello" + name;
+    }
 }
